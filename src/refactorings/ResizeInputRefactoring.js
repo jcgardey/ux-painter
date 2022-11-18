f12bc2c6d1df482ab728f915b56e4ed5106ff490
unknown <felibarreto2013@gmail.com>
2022-11-18 18:07:40 -0300

    ResizeInput with styles

--- src/refactorings/ResizeInputRefactoring.js
+++ src/refactorings/ResizeInputRefactoring.js
@@ -1,12 +1,29 @@
 import UsabilityRefactoringOnElement from './UsabilityRefactoringOnElement';
 
 class ResizeInputRefactoring extends UsabilityRefactoringOnElement {
+  style = [
+    {
+      name: 'Input',
+      properties: {
+        color: 'black',
+        backgroundColor: 'white',
+        textAlign: 'left',
+        marginTop: '0px',
+        marginBottom: '0px',
+        marginRight: '0px',
+        marginLeft: '0px',
+        border: 'none',
+      },
+    },
+  ];
+
   constructor() {
     super();
   }
 
   transform() {
     this.getElement().style.width = this.getInputWidth() + 'px';
+    this.applyStyle(this.getElement(), 'Input');
   }
 
   checkPreconditions() {
