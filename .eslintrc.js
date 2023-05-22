6e457bda76bc1da406c291ebcb79f909f28769bc
juancruzgardey <juancruzgardey@hotmail.com>
2023-05-22 13:33:56 -0300

    fix prettier config to enable unix line-breaking

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'react/react-in-jsx-scope': 'off',
